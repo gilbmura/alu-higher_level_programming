31461dc94039c114c77d256e8f7bd4fb5351b8b1
gilbmura <gilbertmuramira@gmail.com>
2025-03-22 12:40:37 -0700

    python network

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+"""Fetches url
+using the request moduel
 """
-Same as Task 0 but using request package
-"""
-
 import requests
 
 
 if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
